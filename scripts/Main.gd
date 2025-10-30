1330adfa77e9df10b18e594427888737eb9c7f79
tweakrat <angelv8888@gmail.com>
2025-10-30 18:35:35 +1300

    Connecting Job

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -90,3 +90,7 @@ func _on_settings_pressed() -> void:
 
 func _on_btn_care_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/careselector.tscn")
+
+
+func _on_btn_job_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/jobselector.tscn")
